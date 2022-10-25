f25859305c5dbecd0702ffcc3fb4d00f74f9c089
mohamed97-ba <mohamedbenabdallah097@gmail.com>
2022-10-25 20:52:18 +0100

    clean code

--- app/EmployeeApp/models.py
+++ app/EmployeeApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import CharField
 
 from .utils import WorkType, Team
 
@@ -10,15 +11,15 @@ class Employee(models.Model):
     Defines the attributes of an Employee
     """
 
-    name = models.CharField(max_length=100)
-    team = models.CharField(max_length=2, choices=Team.choices(), default=Team.DATA)
-    hourly_rate = models.FloatField(null=False)
+    name = models.CharField(max_length=100) 
+    team = models.CharField(max_length=2, choices=Team.choices(), default=Team.DATA) 
+    hourly_rate = models.FloatField(null=False) 
     is_team_leader = models.BooleanField()
 
-    def get_employee(self):
+    def get_employee(self) -> str:
         return self.name + " belongs to " + self.get_team_name() + " team."
 
-    def get_team_name(self):
+    def get_team_name(self) -> str:
         return Team(self.team).name
 
 
@@ -32,10 +33,10 @@ class Work_Arrangement(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     full_or_partime = models.CharField(
         max_length=2, choices=WorkType.choices(), default=WorkType.FULLTIME
-    )
-    work_rate = models.FloatField()
+    ) # type: str
+    work_rate = models.FloatField() # type: float
 
-    def get_work_type(self):
+    def get_work_type(self) -> str:
         return WorkType(self.full_or_partime).name
 
     @staticmethod
