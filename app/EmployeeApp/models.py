4d33c02e93fc325fa68c7a47f1d267ace045901c
mohamed97-ba <mohamedbenabdallah097@gmail.com>
2022-10-25 21:34:51 +0100

    add test for work_arrangement module

--- app/EmployeeApp/models.py
+++ app/EmployeeApp/models.py
@@ -11,9 +11,9 @@ class Employee(models.Model):
     Defines the attributes of an Employee
     """
 
-    name = models.CharField(max_length=100) 
-    team = models.CharField(max_length=2, choices=Team.choices(), default=Team.DATA) 
-    hourly_rate = models.FloatField(null=False) 
+    name = models.CharField(max_length=100)
+    team = models.CharField(max_length=2, choices=Team.choices(), default=Team.DATA)
+    hourly_rate = models.FloatField(null=False)
     is_team_leader = models.BooleanField()
 
     def get_employee(self) -> str:
@@ -33,11 +33,20 @@ class Work_Arrangement(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
     full_or_partime = models.CharField(
         max_length=2, choices=WorkType.choices(), default=WorkType.FULLTIME
-    ) # type: str
-    work_rate = models.FloatField() # type: float
+    )  # type: str
+    work_rate = models.FloatField()  # type: float
 
     def get_work_type(self) -> str:
         return WorkType(self.full_or_partime).name
+
+    def __str__(self) -> str:
+        return (
+            self.employee.name
+            + " is a "
+            + str(self.work_rate)
+            + " "
+            + self.get_work_type()
+        )
 
     @staticmethod
     def get_jobs_by_employee(employee_id):
--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from EmployeeApp.models import Employee
+from EmployeeApp.models import Employee, Work_Arrangement
 
 
 class EmployeeTest(TestCase):
@@ -20,3 +20,30 @@ class EmployeeTest(TestCase):
             employee_mohamed.get_employee(), "mohamed belongs to DATA team."
         )
         self.assertEqual(employee_hamza.get_employee(), "hamza belongs to DATA team.")
+
+
+class Work_ArrangementTest(TestCase):
+    """Test module for work_arrangement model"""
+
+    def setUp(self):
+        Employee.objects.create(
+            name="mohamed", team="DT", hourly_rate=20, is_team_leader=True
+        )
+        self.employee_1 = Employee.objects.get(name="mohamed")
+
+        Work_Arrangement.objects.create(
+            employee=self.employee_1, full_or_partime="FT", work_rate=100.0
+        )
+        Employee.objects.create(
+            name="hamza", team="DT", hourly_rate=15, is_team_leader=False
+        )
+        self.employee_2 = Employee.objects.get(name="hamza")
+        Work_Arrangement.objects.create(
+            employee=self.employee_2, full_or_partime="PT", work_rate=50.0
+        )
+
+    def test_work_arrangement(self):
+        job1 = Work_Arrangement.objects.get(employee_id=self.employee_1.pk)
+        job2 = Work_Arrangement.objects.get(employee_id=self.employee_2.pk)
+        self.assertEqual(job1.__str__(), "mohamed is a 100.0 FULLTIME")
+        self.assertEqual(job2.__str__(), "hamza is a 50.0 PARTIME")
